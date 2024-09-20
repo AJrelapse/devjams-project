cb28fb9050877dd4086f926939b6c1c335377b5d
S M Ajay Anand <ajayanandsm@gmail.com>
2024-09-21 03:27:10 +0530

    Start Yield

---
+++ my-app/src/components/yieldpredict.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/yield.css";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+
+function Yield() {
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  return (
+    <div className="container1 cards1">
+        <h1>Hello</h1>
+      
+    </div>
+  );
+}
+
+export default Yield;
