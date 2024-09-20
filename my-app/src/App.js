5f75a70396ee23e2d7c70bb5124478b12fa7135b
S M Ajay Anand <ajayanandsm@gmail.com>
2024-09-20 12:17:21 +0530

    Add images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
-import Navbar from "./components/navbar"
+import Navbar from "./components/navbar";
+import Parallax from "./components/parallax";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Parallax />
     </div>
   );
 }
---
+++ my-app/src/components/parallax.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function parallax() {
+  return (
+    <div>
+        <section className="parallax">
+            <img src="../assets/images/hill1.png" id="hill1"></img>
+            <img src="../assets/images/hill2.png" id="hill2"></img>
+            <img src="../assets/images/hill3.png" id="hill3"></img>
+            <img src="../assets/images/hill4.png" id="hill4"></img>
+            <img src="../assets/images/hill5.png" id="hill5"></img>
+            <img src="../assets/images/tree.png" id="tree"></img>
+            <h2 id="text">FARMER's TOOL</h2>
+            <img src="../assets/images/leaf.png" id="leaf"></img>
+            <img src="../assets/images/plant.png" id="plant"></img>
+        </section>
+    </div>
+  )
+} 
+
+export default parallax;
