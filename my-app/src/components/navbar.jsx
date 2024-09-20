2f4240ca8e9f163cc87fc75d7d9521c3f0f507f3
Hemanth Raja <hemanthraja2004@gmail.com>
2024-09-20 16:18:02 +0530

    Navbar and responsive

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,65 +1,55 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-function navbar() {
-  return (<div classNameName='nav'>
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
-          <div className="container">
-          <a className="navbar-brand" href="#!">
-            <span style={{ color: '#5e9693' }}>Debuggers7</span>
-            <span style={{ color: '#fff' }}></span>
-        </a>
+function Navbar() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.querySelector('.custom-navbar');
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    };
 
-            <button className="navbar-toggler" type="button" data-mdb-toggle="collapse"
-              data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="Toggle navigation">
-              <i className="fas fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Offer</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Features</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Portfolio</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Reference</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Team</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#!">Contact</a>
-                </li>
+    window.addEventListener('scroll', handleScroll);
+    
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand px-4 mx-4" href="/home">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Pricing</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown link</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/">Action</a></li>
+                <li><a className="dropdown-item" href="/">Another action</a></li>
+                <li><a className="dropdown-item" href="/">Something else here</a></li>
               </ul>
-              <ul className="navbar-nav d-flex flex-row">
-                <li className="nav-item me-3 me-lg-0">
-                  <a className="nav-link" href="#!">
-                    <i className="fa fa-shopping-cart"></i>
-                  </a>
-                </li>
-                <li className="nav-item me-3 me-lg-0">
-                  <a className="nav-link" href="#!">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li className="nav-item me-3 me-lg-0">
-                  <a className="nav-link" href="#!">
-                    <i className="fa fa-instagram"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-  </div>)
-  
-};
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
-export default navbar;
+export default Navbar;
--- my-app/src/components/parallax.jsx
+++ my-app/src/components/parallax.jsx
@@ -1,51 +1,95 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 function Parallax() {
-  // Create refs to access DOM elements
   const textRef = useRef(null);
   const leafRef = useRef(null);
   const hill1Ref = useRef(null);
   const hill4Ref = useRef(null);
   const hill5Ref = useRef(null);
+  const treeRef = useRef(null); // Reference for the tree
 
   useEffect(() => {
     const handleScroll = () => {
       const value = window.scrollY;
 
-      // Apply parallax effect using refs
-      if (textRef.current) textRef.current.style.marginTop = value * 2.5 + 'px';
-      if (leafRef.current) {
-        leafRef.current.style.top = value * -1.5 + 'px';
-        leafRef.current.style.left = value * 1.5 + 'px';
+      if (window.innerWidth > 768) {
+        if (textRef.current) textRef.current.style.marginTop = value * 2.5 + "px";
+        if (leafRef.current) {
+          leafRef.current.style.top = value * -1.5 + "px";
+          leafRef.current.style.left = value * 1.5 + "px";
+        }
+        if (hill1Ref.current) hill1Ref.current.style.top = value * 1 + "px";
+        if (hill4Ref.current) hill4Ref.current.style.left = value * -1.5 + "px";
+        if (hill5Ref.current) hill5Ref.current.style.left = value * 1.5 + "px";
+        if (treeRef.current) treeRef.current.style.left = value * -0.5 + "px"; // Slower movement for the tree
+      } else {
+        // Reset styles for mobile devices
+        if (textRef.current) textRef.current.style.marginTop = "0px";
+        if (leafRef.current) {
+          leafRef.current.style.top = "0px";
+          leafRef.current.style.left = "0px";
+        }
+        if (hill1Ref.current) hill1Ref.current.style.top = "0px";
+        if (hill4Ref.current) hill4Ref.current.style.left = "0px";
+        if (hill5Ref.current) hill5Ref.current.style.left = "0px";
+        if (treeRef.current) treeRef.current.style.left = "0px"; // Reset tree position
       }
-      if (hill1Ref.current) hill1Ref.current.style.top = value * 1 + 'px';
-      if (hill4Ref.current) hill4Ref.current.style.left = value * -1.5 + 'px';
-      if (hill5Ref.current) hill5Ref.current.style.left = value * 1.5 + 'px';
     };
 
-    // Add the scroll event listener
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    // Cleanup the event listener on component unmount
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <div>
-     
-
       <section className="parallax">
-        <img src="/assets/images/hill1.png" ref={hill1Ref} id="hill1" alt="greenary"></img>
-        <img src="/assets/images/hill2.png" id="hill2" alt="greenary"></img>
-        <img src="/assets/images/hill3.png" id="hill3" alt="greenary"></img>
-        <img src="/assets/images/hill4.png" ref={hill4Ref} id="hill4" alt="greenary"></img>
-        <img src="/assets/images/hill5.png" ref={hill5Ref} id="hill5" alt="greenary"></img>
-        <img src="/assets/images/leaf.png" ref={leafRef} id="leaf" alt="greenary"></img>
-        <img src="/assets/images/tree.png" id="tree" alt="greenary"></img>
-        <h2 id="text" ref={textRef}>FARMER's TOOL</h2>
-        <img src="/assets/images/plant.png" id="plant" alt="greenary"></img>
+        <img
+          src="/assets/images/hill1.png"
+          ref={hill1Ref}
+          id="hill1"
+          alt="greenery"
+        />
+        <img src="/assets/images/hill2.png" id="hill2" alt="greenery" />
+        <img src="/assets/images/hill3.png" id="hill3" alt="greenery" />
+        <img
+          src="/assets/images/hill4.png"
+          ref={hill4Ref}
+          id="hill4"
+          alt="greenery"
+        />
+        <img
+          src="/assets/images/hill5.png"
+          ref={hill5Ref}
+          id="hill5"
+          alt="greenery"
+        />
+        <img
+          src="/assets/images/leaf.png"
+          ref={leafRef}
+          id="leaf"
+          alt="greenery"
+        />
+        <img
+          src="/assets/images/tree.png"
+          ref={treeRef} // Add ref to tree
+          id="tree"
+          alt="greenery"
+        />
+        <h2 id="text" ref={textRef}>
+          FARMER's TOOL
+        </h2>
+        <img src="/assets/images/plant.png" id="plant" alt="greenery" />
+      </section>
+      <section className="sec">
+        <h2>Parallax Scrolling Website</h2>
+        <p>
+          A week ago, a friend invited a couple of other couples over for dinner.
+          Eventually, the food (but not the wine) was cleared off the table for
+          what turned out to be some fierce Scrabbling...
+        </p>
       </section>
     </div>
   );
